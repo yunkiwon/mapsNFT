4543786d5facd63ba18e6928e5d4fa6e3b64e810
Austin Ramos <austinramos@Austins-MacBook-Pro.local>
2022-06-25 20:57:21 -0400

    cleaned index

--- pages/index.js
+++ pages/index.js
@@ -19,52 +19,6 @@ export default function Home() {
   const [mintMessage, setMintMessage] = useState('')
   const [mintLoading, setMintLoading] = useState(false)
 
-  // Call smart contract to mint NFT(s) from current address
-  async function mintNFTs() {
-    // Check quantity
-    if (mintQuantity < 1) {
-      setMintMessage('You need to mint at least 1 NFT.')
-      setMintError(true)
-      mintQuantityInputRef.current.focus()
-      return
-    }
-    if (mintQuantity > MAX_MINT) {
-      setMintMessage('You can only mint a maximum of 10 NFTs.')
-      setMintError(true)
-      mintQuantityInputRef.current.focus()
-      return
-    }
-
-    // Get wallet details
-    if (!hasEthereum()) return
-    try {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner()
-
-      try {
-        const address = await signer.getAddress()
-
-        setMintLoading(true);
-        // Interact with contract
-        const contract = new ethers.Contract(process.env.NEXT_PUBLIC_MINTER_ADDRESS, Minter.abi, signer)
-        const totalPrice = MINT_PRICE * mintQuantity
-        const transaction = await contract.mint(mintQuantity, {value: ethers.utils.parseEther(totalPrice.toString())})
-        await transaction.wait()
-
-        mintQuantityInputRef.current.value = 0
-        setMintMessage(`Congrats, you minted ${mintQuantity} token(s)!`)
-        setMintError(false)
-      } catch {
-        setMintMessage('Connect your wallet first.');
-        setMintError(true)
-      }
-    } catch (error) {
-      setMintMessage(error.message)
-      setMintError(true)
-    }
-    setMintLoading(false)
-  }
-
   // fetch(`${process.env.NFTPORT_BACKEND_URL}${process.env.BLOCKPARTY_CONTRACT_ADDRESS}?chain=ethereum&include=metadata`, {
   //   method: 'GET',
   //   headers: {"Content-Type": 'application/json', Authorization: process.env.NFTPORT_KEY}
